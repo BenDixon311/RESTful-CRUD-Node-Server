3456be5b6508bbccf4812a865edf8356d962856c
Ben Dixon <bendixon311@gmail.com>
2019-02-25 21:58:44 -0700

    updated headers

--- app.js
+++ app.js
@@ -14,6 +14,13 @@ app.use(bodyParser.urlencoded({extended: false, useNewUrlParser: true}));
 
 app.use('/', playerRoutes);
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
+
 
 mongoose.connect('mongodb+srv://Ben:epitaph311@myfirstcluster-1t85r.mongodb.net/players?retryWrites=true', {useNewUrlParser: true})
     .then(result => {

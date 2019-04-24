7dd7b26e2f272f65600e56bc99b67288032f455c
Ben Dixon <bendixon311@gmail.com>
2019-04-23 23:03:05 -0600

    test

--- controllers/edit-player.js
+++ controllers/edit-player.js
@@ -11,7 +11,7 @@ exports.addPlayer = (req, res, next) => {
     player
         .save()
         .then(result => {
-            res.status(201).redirect('http://localhost:3000/');
+            res.status(201).redirect('http://espn.com');
             console.log('Player Created');
             
         })
